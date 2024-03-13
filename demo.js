4c004abbcb4b9143e5f2cfd63f013233d46ba6ba
rahulmagar5455 <112691037+rahulmagar5455@users.noreply.github.com>
2024-03-13 10:52:28 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,12 @@
+// Variable Declarations
+let x, y, sum;
+
+// Assign value to the variables
+x = 3;
+y = 23;
+
+// Use arithmetic operator to
+// add two numbers
+sum = x + y;
+
+console.log(sum);
